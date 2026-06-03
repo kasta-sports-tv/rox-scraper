08c563776c823b54409215f70ca2365078358464
kasta-sports-tv <kasta.sports.tv@gmail.com>
2026-06-04 00:06:21 +0300

    Use random domains for stream URLs in M3U
    Replaced static stream URLs with dynamic URLs using random domains.

--- build_m3u.py
+++ build_m3u.py
@@ -1,9 +1,14 @@
 import json
-import os
+import random
 
-if not os.path.exists("output.json"):
-    print("ERROR: output.json not found")
-    exit(1)
+DOMAINS = [
+    "formaturamaxi.com.br",
+    "sandhost.qzz.io",
+    "thelistener.pk"
+]
+
+SUBDOMAIN = "601"
+
 
 with open("output.json", "r", encoding="utf-8") as f:
     data = json.load(f)
@@ -11,22 +16,17 @@ with open("output.json", "r", encoding="utf-8") as f:
 m3u = "#EXTM3U\n"
 
 for cat, items in data.items():
-    if not isinstance(items, list):
-        continue
-
     for item in items:
         title = item.get("title", "No title")
         streams = item.get("streams") or []
 
-        if not isinstance(streams, list):
-            continue
-
         for s in streams:
-            if not s:
-                continue
+            domain = random.choice(DOMAINS)
+
+            url = f"https://{SUBDOMAIN}.{domain}/{s}"
 
             m3u += f'#EXTINF:-1 group-title="{cat}",{title}\n'
-            m3u += s + "\n"
+            m3u += url + "\n"
 
 with open("playlist.m3u", "w", encoding="utf-8") as f:
     f.write(m3u)
